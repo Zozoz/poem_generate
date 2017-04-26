a2801284b54628fb3f69d3f6a3f649a400ec70b6
Zozoz <zhengshiliang0@gmail.com>
2017-04-26 14:47:29 +0800

    fix a bug

--- tang_poems.py
+++ tang_poems.py
@@ -16,7 +16,7 @@ tf.app.flags.DEFINE_integer('batch_size', 64, 'batch size')
 tf.app.flags.DEFINE_float('lr', 0.01, 'learning rate')
 
 tf.app.flags.DEFINE_string('checkpoints_dir', './checkpoints/poems/', 'checkpoints save path')
-tf.app.flags.DEFINE_string('train_file', './dataset/data/poems.txt', 'train file path')
+tf.app.flags.DEFINE_string('train_file', './data/poems.txt', 'train file path')
 tf.app.flags.DEFINE_string('model_prefix', 'poems', 'model save prefix')
 
 tf.app.flags.DEFINE_integer('epochs', 50, 'training epochs')
